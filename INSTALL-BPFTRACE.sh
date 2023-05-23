e62e63e1a418e2290ca9d357d871c8f08cee7ffe
CSCI-KNOX <knoxd@colorado.edu>
2023-05-23 23:19:46 +0000

    updated packages, removed clone and compile

--- INSTALL-BPFTRACE.sh
+++ INSTALL-BPFTRACE.sh
@@ -6,11 +6,20 @@
 #
 # See https://github.com/iovisor/bpftrace/blob/master/INSTALL.md#ubuntuls
 
+
+echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | \
+sudo tee -a /etc/apt/sources.list.d/ddebs.list
+
+sudo apt install ubuntu-dbgsym-keyring
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F2EDC64DC5AEE1F6B9C621F0C8CAB6595FDFF622
 sudo apt-get update
+
 sudo apt-get install -y \
   bison \
-  cmake \
   flex \
+  cmake \
   libelf-dev \
   zlib1g-dev \
   libfl-dev \
@@ -19,16 +28,15 @@ sudo apt-get install -y \
   libcereal-dev \
   llvm-dev \
   llvm-runtime \
+  liblzma-dev \
   libclang-dev \
   clang \
   libpcap-dev \
   libgtest-dev \
   libgmock-dev \
   asciidoctor \
+  bpftrace \
+  bpftrace-dbgsym \
   pahole
-git clone https://github.com/iovisor/bpftrace --recurse-submodules
-mkdir bpftrace/build; cd bpftrace/build;
-../build-libs.sh
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j4
-sudo make install
+
+
